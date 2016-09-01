e5ef6b1754ca655d8757994c9ade6455d3d7484c
sf <sffaster@126.com>
2016-09-01 11:48:04 +0800

    加入显示天气信息、切换城市和手动更新天气的功能。

--- app/src/main/java/com/sf/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/sf/coolweather/util/HttpUtil.java
@@ -1,15 +1,68 @@
 package com.sf.coolweather.util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.Charset;
+
 
 /**
  * Created by sf on 16-8-31.
  */
 public class HttpUtil {
+    /**
+     * 放弃
+     * while ((line = reader.readLine()) != null)在从服务器获取天气信息时，
+     * 抛出EOF异常，改为字节流读取数据，并获取异常不处理直接返回响应结果
+     * @param address
+     * @param listener
+     */
+    public static void sendHttpRequest_old(final String address, final HttpCallbackListener listener) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(address);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in, Charset.forName("Utf-8")));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    if (listener != null) {
+                        // 回调onFinish()方法
+                        listener.onFinish(response.toString());
+                    }
+                } catch (Exception e) {
+                    if (listener != null) {
+                        // 回调onError()方法
+                        listener.onError(e);
+                    }
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * while ((line = reader.readLine()) != null)在从服务器获取天气信息时，
+     * 抛出EOF异常，改为字节流读取数据，并获取异常不处理直接返回响应结果
+     * @param address
+     * @param listener
+     */
     public static void sendHttpRequest(final String address, final HttpCallbackListener listener) {
         new Thread(new Runnable() {
             @Override
@@ -22,11 +75,21 @@ public class HttpUtil {
                     connection.setConnectTimeout(8000);
                     connection.setReadTimeout(8000);
                     InputStream in = connection.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
                     StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
+
+                    try {
+                        byte[] b = new byte[4096];
+                        for (int n; (n = in.read(b)) != -1;) {
+                            response.append(new String(b, 0, n));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                     if (listener != null) {
